b8f2ae3886d77beaf0e28e7dcc378a3d2e7538ac
Nikolas <nkls99@gmail.com>
2022-06-14 14:52:11 -0300

    token middleware + signIn corrections

--- controllers/authController.js
+++ controllers/authController.js
@@ -16,8 +16,10 @@ async function signIn (req, res) {
             return res.status(401).send('Wrong password');
         }
 
-        const config = { expiresIn: 60*60 };
-        const token = jwt.sign(user, process.env.JWT_SECRET, config);
+        delete result.rows[0].password;
+
+        const config = { expiresIn: 60*60*12 };
+        const token = jwt.sign(result.rows[0], process.env.JWT_SECRET, config);
         return res.send(token).status(200);
 
     } catch (error) {
