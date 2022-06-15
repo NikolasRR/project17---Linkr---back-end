e084d59def190d02909906929205945f35c6770c
Nikolas Rueckmann <nkls99@gmail.com>
2022-06-15 18:08:37 -0300

    Revert "Feature/sign in"

--- index.js
+++ index.js
@@ -1,23 +1,16 @@
 import express, {json} from "express";
 import dotenv from "dotenv";
-import cors from "cors";
-import chalk from "chalk";
-import cookieParser from "cookie-parser";
-
-import authRouter from "./routes/authRoutes.js";
-
 dotenv.config();
+
+import router from "./routes/index.js";
 
 const app = express();
 
-app.use(cookieParser());
-app.use(cors({ credentials: true, origin: ["http://localhost:3000"] }));
 app.use(json());
-
-app.use(authRouter)
+app.use(router);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
-  console.log(chalk.bold.blue(`Mode: ${process.env.MODE || "DEV"}`));
-  console.log(chalk.bold.blue(`Server is up on port: ${port}`));
+  console.log(`Mode: ${process.env.MODE || "DEV"}`);
+  console.log(`Server is up on port: ${port}`);
 });
--- routes/index.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-
-import authRouter from "./authRoutes.js";
-
-const router = Router();
-
-router.use(authRouter);
-
-export default router;
