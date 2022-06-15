64c03fe28e105945d29c6a01b8df91763bda6426
Nikolas <nkls99@gmail.com>
2022-06-15 18:07:45 -0300

    token middleware correction

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express, {json} from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import chalk from "chalk";
+import cookieParser from "cookie-parser";
 
 import authRouter from "./routes/authRoutes.js";
 
@@ -9,8 +10,9 @@ dotenv.config();
 
 const app = express();
 
+app.use(cookieParser());
+app.use(cors({ credentials: true, origin: ["http://localhost:3000"] }));
 app.use(json());
-app.use(cors());
 
 app.use(authRouter)
 
--- middlewares/tokenMiddleware.js
+++ middlewares/tokenMiddleware.js
@@ -1,11 +1,10 @@
 import jwt from "jsonwebtoken";
 
 async function tokenValidation (req, res, next) {
-    const token = req.headers.authorization;
+    const token = req.headers.cookie;
     if (!token) {
         return res.sendStatus(422);
     }
-    const filteredToken = token.replace("Bearer", "").trim();
     try {
         const user = jwt.verify(filteredToken, process.env.JWT_SECRET);
         res.locals.user = user;
