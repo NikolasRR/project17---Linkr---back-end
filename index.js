a5dc06f8e0fe9fe0309c4bb2bb3cbfbb2ff17b90
Victor Matos <antoniovitomatos@gmail.com>
2022-06-20 15:15:51 -0300

    make it better

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ dotenv.config();
 const app = express();
 
 app.use(cookieParser());
-app.use(cors({ credentials: true, origin: ["http://localhost:3000"] }));
+app.use(cors({ credentials: true, origin: ["http://localhost:3000", "https://project17-linkedir-front-mk2ylnha6-nikolasrr.vercel.app/"] }));
 app.use(json());
 
 app.use(hashTagsRouter);
