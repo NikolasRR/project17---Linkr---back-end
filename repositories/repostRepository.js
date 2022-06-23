23291241120c363c5438fc0f9b919f943f31582f
Victor Matos <antoniovitomatos@gmail.com>
2022-06-22 23:14:08 -0300

    ajuste id

--- repositories/repostRepository.js
+++ repositories/repostRepository.js
@@ -24,10 +24,12 @@ async function getReposts(userId){
         `SELECT users.id as "userId", publications.id as "publicationId", publications.content, publications.url, 
         COUNT(likes."publicationId") as "totalLikes", users."userName", users.image as profile, 
         links.id, links.title, links.description, links.image, links.link,
-        (SELECT COALESCE(COUNT("publicationId"), 0) from repost where "publicationId" = publications.id) as resposts, repost."createdAt"
+        (SELECT COALESCE(COUNT("publicationId"), 0) from repost where "publicationId" = publications.id) as resposts, repost."createdAt", repost."userId" as "repostId",u2.id as "localId" , u2."userName" as "repostedBy"
                 FROM publications 
                 JOIN repost
                 ON publications.id = repost."publicationId"
+                LEFT JOIN users u2
+                ON u2.id = repost."userId"
                 LEFT JOIN likes
                 ON publications.id = likes."publicationId"
                 JOIN users 
@@ -35,7 +37,7 @@ async function getReposts(userId){
                 JOIN links
                 ON links.id = publications."linkId"
                 WHERE repost."userId" = $1
-                GROUP BY publications.id,users."userName", users.image,likes."publicationId",links.id,users.id,repost."createdAt"
+                GROUP BY publications.id,users."userName", users.image,likes."publicationId",links.id,users.id,repost."createdAt",  repost."userId",  u2."userName", u2.id
                 ORDER BY  repost."createdAt" DESC`,[userId])
 }
 
