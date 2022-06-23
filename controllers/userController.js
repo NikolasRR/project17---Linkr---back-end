08bd8b2c4c934e0d9384634fae45fd68f6f17b85
Laura Neves <lauracmneves@gmail.com>
2022-06-23 14:03:14 -0300

    feat: userPage fetch posts and reposts done

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,5 @@
 import userRepository from "../repositories/userRepository.js"
+import repostRepository from "./../repositories/repostRepository.js"
 
 export async function getUserPublications(req,res){
 
@@ -53,3 +54,25 @@ export async function deleteFollower(req,res){
     }
 }
 
+export async function getUserPublicationsAndRepublications (req,res){
+    const {id} = req.params;
+    try {
+        const { rows: posts } = await repostRepository.getUserPublications(id);
+        const { rows: reposts} = await repostRepository.getUserReposts(id);
+        
+        for(let i in reposts){
+            reposts[i]={...reposts[i], "isRepost":true}
+        }
+
+        const allPosts = posts.concat(reposts);
+        allPosts.sort((a, b)=> {
+            return b.createdAt - a.createdAt;
+        });      
+       
+        res.status(200).json({ "data": allPosts, "message":"OK"})
+
+    } catch (e) {
+        console.error(e)
+        res.sendStatus(500)
+    }
+}   
--- repositories/repostRepository.js
+++ repositories/repostRepository.js
@@ -1,43 +1,43 @@
 import db from "../config/db.js";
 
-// async function getPublications(userId) {
-//     return await db.query(
-//         `SELECT users.id as "userId", publications.id as "publicationId", publications.content, publications.url, 
-//         COUNT(likes."publicationId") as "totalLikes", users."userName", users.image as profile, links.* ,
-//         (SELECT COALESCE(COUNT("publicationId"), 0) from repost where "publicationId" = publications.id) as resposts
-//         FROM publications 
-//         LEFT JOIN likes
-//         ON publications.id = likes."publicationId"
-//         JOIN users 
-//         ON publications."idUser" = users.id 
-//         JOIN links
-//         ON links.id = publications."linkId"
-//         WHERE publications."idUser" = $1 
-//         GROUP BY publications.id,users."userName", users.image,likes."publicationId",links.id,users.id
-//         ORDER BY  publications."createdAt" DESC `,[userId]
-//     )
+async function getUserPublications(userId) {
+    return await db.query(
+        `SELECT users.id as "userId", publications.id as "publicationId", publications.content, publications.url, 
+        COUNT(likes."publicationId") as "totalLikes", users."userName", users.image as profile, links.* ,
+        (SELECT COALESCE(COUNT("publicationId"), 0) from repost where "publicationId" = publications.id) as resposts
+        FROM publications 
+        LEFT JOIN likes
+        ON publications.id = likes."publicationId"
+        JOIN users 
+        ON publications."idUser" = users.id 
+        JOIN links
+        ON links.id = publications."linkId"
+        WHERE publications."idUser" = $1 
+        GROUP BY publications.id,users."userName", users.image,likes."publicationId",links.id,users.id
+        ORDER BY  publications."createdAt" DESC `,[userId]
+    )
     
-// }
+}
 
-// async function getReposts(userId){
-//     return await db.query(
-//         `SELECT users.id as "userId", publications.id as "publicationId", publications.content, publications.url, 
-//         COUNT(likes."publicationId") as "totalLikes", users."userName", users.image as profile, 
-//         links.id, links.title, links.description, links.image, links.link,
-//         (SELECT COALESCE(COUNT("publicationId"), 0) from repost where "publicationId" = publications.id) as resposts, repost."createdAt"
-//                 FROM publications 
-//                 JOIN repost
-//                 ON publications.id = repost."publicationId"
-//                 LEFT JOIN likes
-//                 ON publications.id = likes."publicationId"
-//                 JOIN users 
-//                 ON publications."idUser" = users.id 
-//                 JOIN links
-//                 ON links.id = publications."linkId"
-//                 WHERE repost."userId" = $1
-//                 GROUP BY publications.id,users."userName", users.image,likes."publicationId",links.id,users.id,repost."createdAt"
-//                 ORDER BY  repost."createdAt" DESC`,[userId])
-// }
+async function getUserReposts(userId){
+    return await db.query(
+        `SELECT users.id as "userId", publications.id as "publicationId", publications.content, publications.url, 
+        COUNT(likes."publicationId") as "totalLikes", users."userName", users.image as profile, 
+        links.id, links.title, links.description, links.image, links.link,
+        (SELECT COALESCE(COUNT("publicationId"), 0) from repost where "publicationId" = publications.id) as resposts, repost."createdAt"
+                FROM publications 
+                JOIN repost
+                ON publications.id = repost."publicationId"
+                LEFT JOIN likes
+                ON publications.id = likes."publicationId"
+                JOIN users 
+                ON publications."idUser" = users.id 
+                JOIN links
+                ON links.id = publications."linkId"
+                WHERE repost."userId" = $1
+                GROUP BY publications.id,users."userName", users.image,likes."publicationId",links.id,users.id,repost."createdAt"
+                ORDER BY  repost."createdAt" DESC`,[userId])
+}
 
 async function getPublications() {
     return await db.query(
@@ -81,7 +81,9 @@ async function getReposts(){
 
 const repostRepository = {
     getPublications,
-    getReposts
+    getReposts,
+    getUserPublications,
+    getUserReposts
 }
 
 export default repostRepository
